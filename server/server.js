badd698e500e50ae43b2a79df3c3b9ca0292f10d
TidalJ <395478675@qq.com>
2024-05-05 22:41:31 +0930

    1

--- server/server.js
+++ server/server.js
@@ -1,52 +1,46 @@
-import express from "express";
-import * as dotenv from "dotenv";
-import cors from "cors";
-import OpenAI from "openai";
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+import OpenAI from 'openai';
 
 dotenv.config();
 
-
 const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY,
+    apiKey: process.env.OPENAI_API_KEY, // This is also the default, can be omitted
+    // organization: 'org-dG9qwGbqD3L1foLUVVFGUy1h',
 });
-
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-
-app.get("/", async (req, res) => {
+app.get('/', async (req, res) => {
     res.status(200).send({
-        message: "Hello from MyGPT",
+        message: 'Hello from CodeX',
     })
-});
+})
 
-app.post("/", async (req, res) => {
+app.post('/', async (req, res) => {
     try {
         const prompt = req.body.prompt;
-        const response = await openai.chat.completions.create({
-            model: "gpt-3.5-turbo-16k-0613",
-            messages: [
-                {
-                    "role": "user",
-                    "content": ""
-                }
-            ],
+
+        const response = await openai.create({
+            model: "gpt-3.5-turbo-instruct",
+            prompt: `${prompt}`,
             temperature: 0,
             max_tokens: 3000,
             top_p: 1,
             frequency_penalty: 0.5,
             presence_penalty: 0,
         });
+
         res.status(200).send({
-            bot: response.data.choices[0].text
-        });
+            bot: response.choices[0].text
+        })
     } catch (error) {
         console.log(error);
-        res.status(500).send({ error });
+        res.status(500).send({ error })
     }
+})
 
-});
-
-app.listen(5000, () => console.log("Server is running on port http://localhost:5000"));
+app.listen(5000, () => console.log('Server is running on port http://localhost:5000'));
